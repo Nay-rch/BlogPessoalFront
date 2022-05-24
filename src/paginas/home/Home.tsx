8694a9d397b34a344c12e5aec91e71d7ed3d8d8e
Nay-rch <nayaraluciadarochasilva@gmail.com>
2022-05-24 20:06:33 -0300

    react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+    return (
+    <>
+    <h1 className='titulo' > Home</h1>
+    <img src="" alt="" />
+    </>
+    // inserir imagem dps
+    )
+}
+
+export default Home
+
